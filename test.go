7c0f73428d9527dc98373e800df7cab660e3d051
Anton Efimenko <reloni@ya.ru>
2019-11-24 20:25:10 +0300

    Handle errors

--- test.go
+++ test.go
@@ -17,13 +17,20 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 	key := "03add4c278668e54404560d4ff48d568"
 	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s", city, key)
+
+	w.Header().Set("Content-Type", "application/json")
+
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		message := fmt.Sprintf("{ \"error\": \"Error calling weather service\", \"message\": \"%s\" }", err.Error())
+		w.Write([]byte(message))
+		glog.Error(err.Error())
+		return
 	}
+
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
 }
 
